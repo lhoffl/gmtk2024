2acdc3d7dce24d85aa4d8ba11db4bb3d1be5c24e
Matthew Hoffman <matt@lhoffl.com>
2024-08-17 12:06:52 -0400

    swap characters via tab or lshift

--- transference/characters/players/scripts/player.gd
+++ transference/characters/players/scripts/player.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends Node2D
 
 @onready var _animated_sprite = $CharacterBody2D/AnimatedSprite2D
@@ -14,6 +15,8 @@ extends Node2D
 @export var mass_modifier = 0.7
 @export var coyote_frames = 10
 
+@export var player_controlled : bool = false
+
 @export var jump_height : float
 @export var jump_time_to_peak : float
 @export var jump_time_to_fall : float
@@ -22,16 +25,20 @@ extends Node2D
 @onready var jump_gravity : float = ((-2.0 * jump_height / (jump_time_to_peak * jump_time_to_peak))) * -1
 @onready var fall_gravity : float = ((-2.0 * jump_height / (jump_time_to_fall * jump_time_to_fall))) * -1
 
-var buddy
+var buddy : Player
 var current_jump_frames = 0
 var current_coyote_frames = 0
 var coyote_active : bool = true
 var can_double_jump : bool = true
+var swap_cooldown = 0
 
 func _ready():
 	_animated_sprite.play("walk_p" + str(player_num))
 	_animated_sprite.flip_h = true
 	_animated_sprite.stop()
+	
+	if player_num == 1:
+		player_controlled = true
 	
 func _physics_process(delta):
 	get_input(delta)
@@ -46,14 +53,24 @@ func apply_gravity(delta):
 	_character_body.velocity.y += get_gravity() * delta
 	
 func _process(delta):
+	swap_cooldown -= 1 * delta
 	set_facing()
 	
 func get_input(delta):
 	
+	if Input.is_action_pressed("player" + str(player_num) + "_mass") and goodToGrow():
+		change_mass(mass_modifier * delta)
+		buddy.change_mass(-mass_modifier * delta)
+	
+	if not player_controlled:
+		return
+	
+	print(player_num)
+	
 	var clampedSpeed = clampf(speed * 1/mass, 0, max_speed)
-	if Input.is_action_pressed("player" + str(player_num) + "_left"):
+	if Input.is_action_pressed("player1_left"):
 		_character_body.velocity.x = lerp(_character_body.velocity.x, -1 * clampedSpeed, delta * 0.5)
-	elif Input.is_action_pressed("player" + str(player_num) + "_right"):
+	elif Input.is_action_pressed("player1_right"):
 		_character_body.velocity.x = lerp(_character_body.velocity.x, clampedSpeed, delta * 0.5)
 	else:
 		_character_body.velocity.x = lerp(_character_body.velocity.x, 0.0, delta * 10)
@@ -68,20 +85,21 @@ func get_input(delta):
 	if coyote_active or current_coyote_frames > 0:
 		current_coyote_frames -= 1
 		
-	if can_double_jump and Input.is_action_just_pressed("player" + str(player_num) + "_jump"):
-		print("HI")
+	if can_double_jump and Input.is_action_just_pressed("player1_jump"):
 		_character_body.velocity.y = jump_velocity
 		can_double_jump = false
 		
-	if current_coyote_frames > 0 and Input.is_action_just_pressed("player" + str(player_num) + "_jump"):
+	if current_coyote_frames > 0 and Input.is_action_just_pressed("player1_jump"):
 		_character_body.velocity.y = jump_velocity
 		coyote_active = false
 		can_double_jump = true
 		current_coyote_frames = 0
-
-	if Input.is_action_pressed("player" + str(player_num) + "_mass") and goodToGrow():
-		change_mass(mass_modifier * delta)
-		buddy.change_mass(-mass_modifier * delta)
+		
+	if Input.is_action_just_released("swapBuddies"):
+		if(buddy.can_swap()):
+			player_controlled = false
+			swap_cooldown = 0.1
+			buddy.player_controlled = true
 		
 func goodToGrow() -> bool:
 	return not (_raycast_up.is_colliding() || _raycast_left.is_colliding() || _raycast_right.is_colliding())
@@ -90,10 +108,13 @@ func grounded() -> bool:
 	return _raycast_leftLegDown.is_colliding() || _raycast_rightLegDown.is_colliding()
 
 func set_facing():
-	if Input.is_action_pressed("player" + str(player_num) + "_left"):
+	if not player_controlled:
+		return
+	
+	if Input.is_action_pressed("player1_left"):
 		_animated_sprite.play("walk_p" + str(player_num))
 		_animated_sprite.flip_h = false
-	elif Input.is_action_pressed("player" + str(player_num) + "_right"):
+	elif Input.is_action_pressed("player1_right"):
 		_animated_sprite.play("walk_p" + str(player_num))
 		_animated_sprite.flip_h = true
 	else:
@@ -105,8 +126,13 @@ func set_facing():
 func change_mass(amount):
 	
 	# NOTE: could be fun to change mass in air but it currently allows you to break through the floor so leave disable for now
-	if !grounded() || !buddy.grounded():
-		return
-	mass = clamp((mass + amount), 0.4, 1.9)
+	#if !grounded() || !buddy.grounded():
+	#	return
+	mass = clamp((mass + amount), 0.3, 1.9)
 	_character_body.scale = Vector2(mass, mass)
 	
+func becomePlayerControlled():
+	player_controlled = true
+
+func can_swap():
+	return swap_cooldown < 0
