7d66f3fa409cf5c38a68577e2c9ddcac8b0186b7
Matthew Hoffman <matt@lhoffl.com>
2024-08-17 11:10:18 -0400

    removed the monkey

--- gmtk2024/lhoffl/Monkey/MonkeyState.gd
+++
@@ -1,8 +0,0 @@
-class_name MonkeyState
-extends Node3D
-
-@export var speed : float
-@export var gravity : float
-@export var mass : float
-@export var monkeyScale : float
-@export var jumpForce : float
