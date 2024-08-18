4512c7193fed19a7efe16dd665fda78b8fd1cebf
BurnsAndy <andy@burnsandrew.com>
2024-08-18 16:36:53 -0400

    Better tree behavior

--- transference/environment/Tree/tree.gd
+++ transference/environment/Tree/tree.gd
@@ -6,14 +6,16 @@ extends Node2D
 
 @onready var _trunk = $Trunk
 @onready var _frond = $Fronds_StaticBody
+@onready var _frond_sprite = $Fronds_StaticBody/Sprite2D
 @export var _tree_height = 4
+@export var _tree_stand_delay = 300
 
 @export var _rightBendAmount = 90
 @export var _leftBendAmount = -90
 @export var _requiredMass = 1.5
-@export var _neutralBendAmount = 45
+@export var _neutralBendAmount = 0
 
-enum treeStates {STAND, BEND, BEND_UP, BEND_DOWN}
+enum treeStates {STAND, BEND, BEND_UP, BEND_DOWN, BENT}
 var treeState
 @export var rotation_speed = 1.5
 @export var return_speed = 10
@@ -28,6 +30,8 @@ var bendDegree = 0
 var is_bending = false
 var is_bending_right = false
 var player
+var current_stand_delay
+@onready var targetFrondRotationDegrees = 0.0
 
 func _ready():
 	
@@ -42,6 +46,8 @@ func _ready():
 	
 	buildTree()
 	treeState = treeStates.STAND
+	current_stand_delay = _tree_stand_delay
+	_frond_sprite.rotation = 0.0
 	
 func buildTree():
 	for n in _tree_height:
@@ -65,41 +71,58 @@ func _physics_process(delta):
 			rightCount += 1
 			player = rightRay.get_collider().get_parent()
 	
-	if (leftCount > 0 or rightCount > 0) and player and player.mass > _requiredMass:			
-		if not is_bending:
-			is_bending = true
-			if(rightCount > leftCount):
-				is_bending_right = true
-				bendToState(treeStates.BEND, delta, _rightBendAmount)
-			elif(leftCount > rightCount):
-				is_bending_right = false
-				bendToState(treeStates.BEND, delta, _leftBendAmount)
-		else:
+	if treeState not in [treeStates.BEND, treeStates.BENT] and (leftCount > 0 or rightCount > 0) and player and player.mass > _requiredMass:			
+		treeState = treeStates.BEND
+		if(rightCount > leftCount):
+			is_bending_right = true
+		elif(leftCount > rightCount):
+			is_bending_right = false
+	elif treeState == treeStates.BENT:
+		current_stand_delay -= 1
+		if current_stand_delay <= 0:
+			current_stand_delay = _tree_stand_delay
+			treeState = treeStates.STAND
+			
+	match treeState:
+		treeStates.STAND:
+			bendToState(treeStates.STAND, delta, _neutralBendAmount)
+		treeStates.BEND:
 			if is_bending_right:
 				bendToState(treeStates.BEND, delta, _rightBendAmount)
 			else:
 				bendToState(treeStates.BEND, delta, _leftBendAmount)
-	else:
-		is_bending = false
-		bendToState(treeStates.STAND, delta, _neutralBendAmount)
-		
+				
+	print(treeStates.keys()[treeState] + " Rot: " + str(_trunk.rotation_degrees) + " Delay: " + str(current_stand_delay) + " Frond Rot: " + str(_frond_sprite.rotation))
 	positionFrond()
+	shakeFrond(delta)
 
 func positionFrond():
 	_frond.global_position.x = top_trunk.global_position.x
 	_frond.global_position.y = top_trunk.global_position.y - 4
+	
+func shakeFrond(delta):
+	#var rotateLeft = -15.0
+	#var rotateRight = 15.0
+	##if current_stand_delay / _tree_stand_delay < .3:
+	#if targetFrondRotationDegrees == 0.0:
+		#targetFrondRotationDegrees = rotateLeft
+	#
+	#if _frond_sprite.rotation == deg_to_rad(rotateLeft):
+		#targetFrondRotationDegrees = rotateRight
+	#elif _frond_sprite.rotation == deg_to_rad(rotateRight):
+		#targetFrondRotationDegrees = rotateLeft
+	##else:
+		##targetFrondRotationDegrees = 0.0
+		#
+	#_frond_sprite.rotation = lerp(_frond_sprite.rotation, deg_to_rad(targetFrondRotationDegrees), delta)
+	pass
 
 func bendToState(newState, delta, bendDegree):
-	if not treeState == newState:
-		if newState	 == treeStates.BEND:
-			if _trunk.rotation == deg_to_rad(bendDegree):
-				treeState = treeStates.BEND
-			else:
-				treeState = treeStates.BEND_DOWN
-			_trunk.rotation = lerp(_trunk.rotation, deg_to_rad(bendDegree), delta * rotation_speed)
-		if newState	 == treeStates.STAND:
-			if _trunk.rotation == deg_to_rad(_neutralBendAmount):
-				treeState = treeStates.STAND
-			else:
-				treeState = treeStates.BEND_UP
-			_trunk.rotation = lerp(_trunk.rotation, deg_to_rad(0), delta * return_speed)
+	if newState	 == treeStates.BEND:
+		if abs(_trunk.rotation - deg_to_rad(bendDegree)) < 0.1:
+			treeState = treeStates.BENT
+		_trunk.rotation = lerp(_trunk.rotation, deg_to_rad(bendDegree), delta * rotation_speed)
+	if newState	 == treeStates.STAND:
+		if _trunk.rotation == deg_to_rad(_neutralBendAmount):
+			treeState = treeStates.STAND
+		_trunk.rotation = lerp(_trunk.rotation, deg_to_rad(0), delta * return_speed)
